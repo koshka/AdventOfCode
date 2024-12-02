147bbc54bd6526ab7af7ca7646062fbb0f149fd8
Masha Meteleva <meteleva.m.a@gmail.com>
2024-12-01 16:49:39 -0800

    Renamed directory

---
+++ 2024/Day 1/solution2.js
@@ -0,0 +1,23 @@
+const { parseInput } = require("./parseInput");
+
+let [list1, list2] = parseInput();
+
+// Solution
+const count = {};
+for (let i = 0; i < list2.length; i++) {
+  const num = list2[i];
+  if (count[num] == null) {
+    count[num] = 1;
+  } else {
+    count[num]++;
+  }
+}
+
+let result = 0;
+for (let i = 0; i < list1.length; i++) {
+  const num = list1[i];
+  const numCount = count[num] ?? 0;
+  result += num * numCount;
+}
+
+console.log("result", result);
